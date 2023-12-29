da13f96d634a0cd2da4a83cc24d32a81690ecc6d
Fernando R. Ramos <150261188+ramosvst89@users.noreply.github.com>
2023-12-29 11:09:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,18 @@
-var map;
-var markers = [];
-var usuarios = []; // Array para armazenar informações sobre os usuários
+// js/script.js
 
+// Função para inicializar o mapa
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16;
-    });
+    // Configurações iniciais do mapa
+    var mapOptions = {
+        center: { lat: -23.550520, lng: -46.633307 }, // Coordenadas iniciais (pode ser qualquer lugar)
+        zoom: 10, // Nível de zoom inicial
+    };
 
-    // Adicionar um marcador para a própria posição
-    adicionarMarcadorMinhaPosicao();
+    // Criação do mapa
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Adicione seu código para obter a localização dos usuários e marcá-los no mapa aqui
 }
 
-function adicionarMarcadorMinhaPosicao() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var minhaPosicao = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            map.setCenter(minhaPosicao);
-
-            // Adicionar marcador
-            adicionarMarcador(minhaPosicao.lat, minhaPosicao.lng, 'Minha Posição', true);
-        }, function () {
-            handleLocationError(true);
-        });
-    } else {
-        // Browser não suporta geolocalização
-        handleLocationError(false);
-    }
-}
-
-function adicionarUsuario() {
-    var nome = document.getElementById('nome').value;
-    
-    if (nome.trim() === "") {
-        alert("Por favor, insira seu nome.");
-        return;
-    }
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var usuarioPosicao = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            // Adicionar marcador com nome do usuário
-            adicionarMarcador(usuarioPosicao.lat, usuarioPosicao.lng, nome, false);
-        }, function () {
-            handleLocationError(true);
-        });
-    } else {
-        // Browser não suporta geolocalização
-        handleLocationError(false);
-    }
-}
-
-function adicionarMarcador(lat, lng, titulo, minhaPosicao) {
-    var marcador = new google.maps.Marker({
-        position: { lat: lat, lng: lng },
-        map: map,
-        title: titulo
-    });
-
-    markers.push(marcador);
-
-    if (!minhaPosicao) {
-        // Adicionar informações do usuário ao array
-        usuarios.push({
-            nome: titulo,
-            lat: lat,
-            lng: lng
-        });
-    }
-}
-
-function handleLocationError(geolocationAvailable) {
-    var erro = geolocationAvailable ?
-        'Erro: O serviço de geolocalização falhou.' :
-        'Erro: Seu navegador não suporta geolocalização.';
-    alert(erro);
-}
+// Chamada para a função de inicialização do mapa quando a página carrega
+google.maps.event.addDomListener(window, 'load', initMap);
